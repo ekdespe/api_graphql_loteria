c9f4a6330d6ef3f4f3dda8089856b98858025ce1
Erik <ekdespe@gmail.com>
2019-06-17 20:44:29 -0300

    fix date headache format

--- app.py
+++ app.py
@@ -2,10 +2,12 @@ from flask import Flask
 from flask_graphql import GraphQLView
 from schema import schema
 import database
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 app.debug = True
-
 
 app.add_url_rule(
     '/graphql',
--- schema.py
+++ schema.py
@@ -1,16 +1,9 @@
 import graphene
+import datetime
 from graphene.relay import Node
 from graphene_mongo import MongoengineConnectionField, MongoengineObjectType
 from models import Resultado_Quina as Resultado_Model
 from models import Cidades as CidadesModel
-from models import Users as UsersModel
-
-
-class Users(MongoengineObjectType):
-    class Meta:
-        model = UsersModel
-        interfaces = (Node,)
-
 
 
 class Cidades(MongoengineObjectType):
@@ -19,14 +12,11 @@ class Cidades(MongoengineObjectType):
         model = CidadesModel
         interfaces = (Node,)
 
-
-
 class Resultado_Quina(MongoengineObjectType):
 
     class Meta:
         model = Resultado_Model
         interfaces = (Node,)
-
 
 
 
@@ -38,4 +28,4 @@ class Query(graphene.ObjectType):
 
 
     
-schema = graphene.Schema(query=Query, types=[Resultado_Quina,Cidades,Users])
+schema = graphene.Schema(query=Query, types=[Resultado_Quina])
